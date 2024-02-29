a4a069af1f3589f9acbca7d874b2e310fab6a6fb
anapinheirood <140964012+anapinheirood@users.noreply.github.com>
2024-02-29 17:18:12 -0300

    Update B&W.js

--- B&W.js
+++ B&W.js
@@ -1,1 +1,11 @@
+const container = document.getElementById('container');
+const cadastroBtn = document.getElementById('cadastro');
+const iniciarsessãoBtn = document.getElementById('iniciar sessão');
 
+cadastroBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+iniciarsessãoBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
